8fdba278b68d8a0c4e151cb7523bf24fdbe62aae
Slovar21 <stanko.tsep@yandex.ru>
2025-05-17 03:17:22 +0700

    16_06init1

--- tests.js
+++ tests.js
@@ -1,2 +1,17 @@
 //добавление участника хакатона
+{
+  "data": {
+    "avatar": "hack22/avatars/1.png",
+    "email": "79635052626@yandex.ru",
+    "first_name": "Слава222",
+    "gender": "male",
+    "intro": "Проебал хакатон 6 лет назад, беру реванш.",
+    "stack": "Могу всё.",
+    "team_id": 1,
+    "telegram": "slovarbrn",
+    "vk": "slovarbrn"
+  },
+  "command": "putPlayerRow"
+}
+
 
